05a2d58d3167a91dc47f5288511827fa1935871b
Proprogam <k.kartal9595@gmail.com>
2023-03-02 11:32:57 +0300

    güncelle2

--- app/[[...category]]/loading.js
+++ app/[[...category]]/loading.js
@@ -1,7 +1,18 @@
+import CategoriesLoading from "@/components/categories/loading";
+import FeatureMovieLoading from "@/components/featured-movie/loading";
+import MoviesSectionLoading from "@/components/movies-section/loading";
 import React from "react";
 
 function Loading() {
-  return <div>Loading</div>;
+  return (
+    <div>
+      <FeatureMovieLoading />
+      <CategoriesLoading />
+      <MoviesSectionLoading />
+      <MoviesSectionLoading />
+      <MoviesSectionLoading />
+    </div>
+  );
 }
 
 export default Loading;
