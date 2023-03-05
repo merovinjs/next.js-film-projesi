d08c18221ff5016f67746157268acd120969dffc
Proprogam <k.kartal9595@gmail.com>
2023-03-05 21:39:36 +0300

    search bar added

---
+++ app/search/[keyword]/page.js
@@ -0,0 +1,34 @@
+import React from "react";
+import styles from "./styles.module.css";
+import Link from "next/link";
+import Image from "next/image";
+const Page = async ({ params, title, movies }) => {
+  const keyword = params.keyword;
+  const res = await fetch(
+    `https://api.themoviedb.org/3/search/movie?api_key=${process.env.API_KEY}&query=${keyword}&language=en-US&page=1&include_adult=false`
+  );
+  const data = await res.json();
+
+  return (
+    <div className={styles.moviesSection}>
+      <h2>Arama Sonuçlarınız</h2>
+      <h3 className={styles.title}>{title}</h3>
+      <div className={styles.movies}>
+        {data.results.map((movie) => (
+          <div className={styles.movie} key={movie.id}>
+            <Link href={`/movie/${movie.id}`}>
+              <Image
+                fill
+                unoptimized
+                alt={movie.title}
+                src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
+              />
+            </Link>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Page;
--- components/header/index.js
+++ components/header/index.js
@@ -1,9 +1,27 @@
+"use client";
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
+import { BiSearch } from "react-icons/bi";
 
 import { FaPlayCircle } from "react-icons/fa";
 import styles from "./styles.module.css";
+import { useRouter } from "next/navigation";
+
 const Header = () => {
+  const [keyword, setKeyword] = useState("");
+  const router = useRouter();
+  const searchFunc = (e) => {
+    if (e.key === "Enter" && keyword.length > 2) {
+      router.push(`/search/${keyword}`);
+      setKeyword("");
+    }
+  };
+  const searchFuncIcon = () => {
+    if (keyword.length > 2) {
+      router.push(`/search/${keyword}`);
+      setKeyword("");
+    }
+  };
   return (
     <header className={`${styles.header} container fluid`}>
       <div className={styles.headerWrapper}>
@@ -17,6 +35,22 @@ const Header = () => {
           <Link href="/">Series</Link>
           <Link href="/">Kids</Link>
         </nav>
+      </div>
+      <div className={styles.baseInput}>
+        <input
+          value={keyword}
+          onKeyDown={searchFunc}
+          onChange={(e) => setKeyword(e.target.value)}
+          type="text"
+          id="text"
+          className={styles.input}
+          placeholder="Film Ara    ! ! !"
+        />
+        <BiSearch
+          onChange={(e) => setKeyword(e.target.value)}
+          onClick={searchFuncIcon}
+          size={25}
+        />
       </div>
     </header>
   );
